52bb532e0a3a37e630447537423d3c0cd9a0a527
Thanya Begum <tbegum@princeton.edu>
2021-06-15 16:14:09 -0400

    Start project plan

---
+++ scripts.js
@@ -0,0 +1,16 @@
+/* CLASSES:
+- game.js for game logic (starting game, checking if there is a winner, alternating players)
+- board.js for 
+- checker.js for the checkers (21 of each color)
+- 
+*/
+
+/* USER FLOW:
+- Screen 1: Name of game with a button. 
+- On click of button, create new game and replace button with the game board
+
+- Screen 2: Game board. 
+- First player prompted that it's their turn
+- Hovering over column highlights that column, shows checker there
+- Clicking on spot drops checker
+*/
